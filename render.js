214901f0c2cd72abaacc0ff7d83de68bfdade25b
naurqu <Nathan.Urquhart@sas.com>
2021-07-21 09:08:58 -0400

    changed comment

--- render.js
+++ render.js
@@ -7,7 +7,7 @@
  * data defined in data.js.
  */
 
-// a final comment 
+// a better comment 
 
 /**
  * Given a hero object (see data.js), this function generates a "card" showing
@@ -34,7 +34,7 @@ export const renderHeroCard = function(hero) {
     
 };
 
-// changed comment 
+// a new comment
 
 
 /**
